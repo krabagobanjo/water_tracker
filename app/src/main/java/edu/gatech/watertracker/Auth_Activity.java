47649eeb822665ecb2e68f695c8bb3ba21dccf28
Kyle Rabago-Banjo <krabagobanjo@outlook.com>
2016-04-22 07:40:01 -0400

    I did a thing?

---
+++ app/src/main/java/edu/gatech/watertracker/Auth_Activity.java
@@ -0,0 +1,53 @@
+package edu.gatech.watertracker;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.api.client.auth.oauth2.BearerToken;
+import com.google.api.client.auth.oauth2.ClientParametersAuthentication;
+import com.google.api.client.auth.oauth2.Credential;
+
+
+public class Auth_Activity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+    public static class AuthClass {
+        private final OAuth oauth;
+        public AuthClass(FragmentActivity activity) {
+            oauth = OAuth.newInstance(activity.getApplicationContext(),
+                    activity.getSupportFragmentManager(),
+                    new ClientParametersAuthentication(Auth_Constants.CLIENT_ID, null),
+                    Auth_Constants.AUTH_URL,
+                    Auth_Constants.TOKEN_URL,
+                    Auth_Constants.REDIRECT_URL,
+                    Auth_Constants.SCOPES
+                    );
+        }
+        public Credential authorize() throws Exception {
+            return oauth.authorizeImplicitly("test").getResult();
+        }
+    }
+}
---
+++ app/src/main/java/edu/gatech/watertracker/Auth_Constants.java
@@ -0,0 +1,17 @@
+package edu.gatech.watertracker;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Kyle on 4/19/2016.
+ */
+public class Auth_Constants {
+    public static final String CLIENT_ID = "227Q8R";
+    public static final String CLIENT_SECRET = "83f4eefcd054b13117e50e07208ec6b7";
+    public static final String AUTH_URL = "https://www.fitbit.com/oauth2/authorize";
+    public static final String TOKEN_URL = "https://api.fitbit.com/oauth2/token";
+    public static final String REDIRECT_URL = "http://localhost/Callback";
+    public static final List<String> SCOPES = Arrays.asList("nutrition");
+    private Auth_Constants() {}
+}
