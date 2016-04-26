5f80a462174b873cf9991408f904e74e9664ea4e
Kyle Rabago-Banjo <Kyle Rabago-Banjo>
2016-04-26 11:42:28 -0400

    login screen shows but still fails

--- app/src/main/java/edu/gatech/watertracker/Auth_Activity.java
+++ app/src/main/java/edu/gatech/watertracker/Auth_Activity.java
@@ -20,45 +20,266 @@ import android.webkit.CookieSyncManager;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.util.Lists;
+import com.wuman.android.auth.AuthorizationDialogController;
+import com.wuman.android.auth.AuthorizationFlow;
+import com.wuman.android.auth.DialogFragmentController;
+import com.wuman.android.auth.OAuthManager;
+
 import com.google.api.client.auth.oauth2.BearerToken;
 import com.google.api.client.auth.oauth2.ClientParametersAuthentication;
 import com.google.api.client.auth.oauth2.Credential;
+import com.wuman.android.auth.oauth2.store.SharedPreferencesCredentialStore;
+import edu.gatech.watertracker.AsyncResourceLoader.Result;
+
+import java.io.IOException;
 
 
 public class Auth_Activity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         super.onCreate(savedInstanceState);
-        AuthClass auth = new AuthClass(this);
-        Credential cred;
 
-        try {
-            cred = auth.authorize();
-        } catch (Exception oops) {
-            cred = null;
-            System.out.println(oops);
+        FragmentManager fm = getSupportFragmentManager();
+
+        if (fm.findFragmentById(android.R.id.content) == null) {
+            OAuthFragment list = new OAuthFragment();
+            fm.beginTransaction().add(android.R.id.content, list).commit();
         }
 
-        System.out.println(cred);
+    }
+    public static class OAuthFragment extends Fragment implements
+            LoaderManager.LoaderCallbacks<Result<Credential>> {
 
+        private static final int LOADER_GET_TOKEN = 0;
+        private static final int LOADER_DELETE_TOKEN = 1;
+
+        private OAuthManager oauth;
+
+        private Button button;
+        private TextView message;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setHasOptionsMenu(true);
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            super.onCreateOptionsMenu(menu, inflater);
+//            inflater.inflate(R.menu.delete_cookies_menu, menu);
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+//            switch (item.getItemId()) {
+//                case R.id.delete_cookies: {
+//                    CookieSyncManager.createInstance(getActivity());
+//                    CookieManager cookieManager = CookieManager.getInstance();
+//                    cookieManager.removeAllCookie();
+//                    return true;
+//                }
+//                default: {
+//                    return super.onOptionsItemSelected(item);
+//                }
+//            }
+            return false;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.oauth_login, container, false);
+        }
+
+        @Override
+        public void onViewCreated(View view, Bundle savedInstanceState) {
+            button = (Button) view.findViewById(android.R.id.button1);
+            setButtonText(R.string.get_token);
+            message = (TextView) view.findViewById(android.R.id.text1);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (v.getTag().equals(R.string.get_token)) {
+                        if (getLoaderManager().getLoader(LOADER_GET_TOKEN) == null) {
+                            getLoaderManager().initLoader(LOADER_GET_TOKEN, null,
+                                    OAuthFragment.this);
+                        } else {
+                            getLoaderManager().restartLoader(LOADER_GET_TOKEN, null,
+                                    OAuthFragment.this);
+                        }
+                    } else { // R.string.delete_token
+                        if (getLoaderManager().getLoader(LOADER_DELETE_TOKEN) == null) {
+                            getLoaderManager().initLoader(LOADER_DELETE_TOKEN, null,
+                                    OAuthFragment.this);
+                        } else {
+                            getLoaderManager().restartLoader(LOADER_DELETE_TOKEN, null,
+                                    OAuthFragment.this);
+                        }
+                    }
+                }
+            });
+        }
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            boolean fullScreen = getActivity().getSharedPreferences("Preference", 0)
+                    .getBoolean("auth_mode", false);
+            // setup credential store
+            SharedPreferencesCredentialStore credentialStore =
+                    new SharedPreferencesCredentialStore(getActivity(),
+                            "oauth", OAuth.JSON_FACTORY);
+            // setup authorization flow
+            AuthorizationFlow flow = new AuthorizationFlow.Builder(
+                    BearerToken.authorizationHeaderAccessMethod(),
+                    OAuth.HTTP_TRANSPORT,
+                    OAuth.JSON_FACTORY,
+                    new GenericUrl(Auth_Constants.TOKEN_URL),
+                    new ClientParametersAuthentication(Auth_Constants.CLIENT_ID, null),
+                    Auth_Constants.CLIENT_ID,
+                    Auth_Constants.AUTH_URL)
+                    .setScopes(Lists.<String> newArrayList())
+                    .setCredentialStore(credentialStore)
+                    .build();
+            // setup UI controller
+            AuthorizationDialogController controller =
+                    new DialogFragmentController(getFragmentManager(), fullScreen) {
+                        @Override
+                        public String getRedirectUri() throws IOException {
+                            return Auth_Constants.REDIRECT_URL;
+                        }
+
+                        @Override
+                        public boolean isJavascriptEnabledForWebView() {
+                            return true;
+                        }
+
+                        @Override
+                        public boolean disableWebViewCache() {
+                            return false;
+                        }
+
+                        @Override
+                        public boolean removePreviousCookie() {
+                            return false;
+                        }
+
+                    };
+            // instantiate an OAuthManager instance
+            oauth = new OAuthManager(flow, controller);
+        }
+
+        @Override
+        public Loader<Result<Credential>> onCreateLoader(int id, Bundle args) {
+            getActivity().setProgressBarIndeterminateVisibility(true);
+            button.setEnabled(false);
+            message.setText("");
+            if (id == LOADER_GET_TOKEN) {
+                return new GetTokenLoader(getActivity(), oauth);
+            } else {
+                return new DeleteTokenLoader(getActivity(), oauth);
+            }
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Result<Credential>> loader,
+                                   Result<Credential> result) {
+            if (loader.getId() == LOADER_GET_TOKEN) {
+                message.setText(result.success ? result.data.getAccessToken() : "");
+            } else {
+                message.setText("");
+            }
+            if (result.success) {
+                if (loader.getId() == LOADER_GET_TOKEN) {
+                    setButtonText(R.string.delete_token);
+                } else {
+                    setButtonText(R.string.get_token);
+                }
+            } else {
+                setButtonText(R.string.get_token);
+//                Crouton.makeText(getActivity(), result.errorMessage, Style.ALERT).show();
+            }
+            getActivity().setProgressBarIndeterminateVisibility(false);
+            button.setEnabled(true);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Result<Credential>> loader) {
+            message.setText("");
+            getActivity().setProgressBarIndeterminateVisibility(false);
+            button.setEnabled(true);
+        }
+
+        @Override
+        public void onDestroy() {
+            getLoaderManager().destroyLoader(LOADER_GET_TOKEN);
+            getLoaderManager().destroyLoader(LOADER_DELETE_TOKEN);
+            super.onDestroy();
+        }
+
+        private void setButtonText(int action) {
+            button.setText(action);
+            button.setTag(action);
+        }
+
+        private static class GetTokenLoader extends AsyncResourceLoader<Credential> {
+
+            private final OAuthManager oauth;
+
+            public GetTokenLoader(Context context, OAuthManager oauth) {
+                super(context);
+                this.oauth = oauth;
+            }
+
+            @Override
+            public Credential loadResourceInBackground() throws Exception {
+                Credential credential =
+                        oauth.authorizeImplicitly(getContext().getString(R.string.token_fitbit_implicit),
+                                null, null).getResult();
+//                LOGGER.info("token: " + credential.getAccessToken());
+                return credential;
+            }
+
+            @Override
+            public void updateErrorStateIfApplicable(AsyncResourceLoader.Result<Credential> result) {
+                Credential data = result.data;
+                result.success = !TextUtils.isEmpty(data.getAccessToken());
+                result.errorMessage = result.success ? null : "error";
+            }
+
+        }
+
+        private static class DeleteTokenLoader extends AsyncResourceLoader<Credential> {
+
+            private final OAuthManager oauth;
+            private boolean success;
+
+            public DeleteTokenLoader(Context context, OAuthManager oauth) {
+                super(context);
+                this.oauth = oauth;
+            }
+
+            @Override
+            public Credential loadResourceInBackground() throws Exception {
+                success = oauth.deleteCredential(getContext().getString(R.string.token_fitbit_implicit),
+                        null, null).getResult();
+//                LOGGER.info("token deleted: " + success);
+                return null;
+            }
+
+            @Override
+            public void updateErrorStateIfApplicable(Result<Credential> result) {
+                result.success = success;
+                result.errorMessage = result.success ? null : "error";
+            }
+
+        }
 
     }
 
-    public static class AuthClass {
-        private final OAuth oauth;
-        public AuthClass(FragmentActivity activity) {
-            oauth = OAuth.newInstance(activity.getApplicationContext(),
-                    activity.getSupportFragmentManager(),
-                    new ClientParametersAuthentication(Auth_Constants.CLIENT_ID, Auth_Constants.CLIENT_SECRET),
-                    Auth_Constants.AUTH_URL,
-                    Auth_Constants.TOKEN_URL,
-                    Auth_Constants.REDIRECT_URL,
-                    Auth_Constants.SCOPES
-                    );
-        }
-        public Credential authorize() throws Exception {
-            return oauth.authorizeImplicitly(Auth_Constants.CLIENT_ID).getResult();
-        }
-    }
 }
